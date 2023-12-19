e51910957c9a7471d8a9ffbc1769ff2ba5cb051e
I549512 <amos.dinh@sap.com>
2023-12-19 15:25:17 +0100

    a

--- test.py
+++ test.py
@@ -1,83 +1,96 @@
+
 import cv2
 import numpy as np
-import random
 
-def draw_random_white_curves(image_size, num_curves, max_curve_length, max_curve_angle, curve_thickness, black_noise_intensity):
-    # Create a blank black image
-    img = np.zeros((image_size[1], image_size[0], 3), dtype=np.uint8)
-    num_curves = random.randint(num_curves[0], num_curves[1])
-    for _ in range(num_curves):
-        # Generate random number of points for the curve
-        num_points = random.randint(3, max_curve_length)
+def get_minimap(img):
+    x, y, width, height = 24, 482, 214, 214
+    minimap = img[y:y + height, x:x + width]
+    return minimap
+
+def trace_walkable_sections(image_path):
+    # Read the image
+    img = cv2.imread(image_path)
+    img = get_minimap(img)
+    
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    # Define lower and upper bounds for blue color (you may need to adjust these values)
+    lower_blue = np.array([76, 49, 168])
+    upper_blue = np.array([98, 246, 247])
+
+    # Thresholding to get binary image
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    
+
+    # Optional: Apply morphological operations for noise reduction
+    #kernel = np.ones((5, 5), np.uint8)
+    #mask = cv2.erode(mask, kernel, iterations=1)
+    #mask = cv2.dilate(mask, kernel, iterations=1)
+
+    # Find contours
+    #contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draw contours on a blank image
+    #result = np.zeros_like(img)
+    result = cv2.bitwise_and(img, img, mask=mask)
+    result[np.where((result != [0,0,0]).all(axis = 2))] = [255,255,255]
+    gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
+
+    # get conours in gray image
+    contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    # draw contours on black image
+    black = np.zeros_like(gray)
+    #cv2.drawContours(black, contours, -1, (255,255,255), 1)
+  
+    # for contour in contours:
+    #     if len(contour) < 5:
+    #         continue
+    #     # Fit an ellipse to the contour
+    #     ellipse = cv2.fitEllipse(contour)
         
-        # Generate random starting angle for the curve
-        start_angle = random.uniform(0, 2 * np.pi)
+    #     # Extract the angle from the ellipse
+    #     angle = ellipse[2]
 
-        # Generate random segment lengths and angles
-        segments = []
-        for _ in range(num_points):
-            segment_length = random.randint(30, 40)  # Adjust the range as needed
-            segment_angle = random.uniform(-max_curve_angle, max_curve_angle)
-            segments.append((segment_length, segment_angle))
+    #     # Draw the ellipse on the original image
+        
+    #     cv2.ellipse(img, ellipse, (0, 255, 0), 2)
 
-        # Calculate curve points
-        points = []
-        current_point = (random.randint(0, image_size[0]), random.randint(0, image_size[1]))
-        points.append(current_point)
+    #     # Display the angle
+    #     print(f"Contour angle: {angle} degrees")
+    for contour in contours:
+        # Filter out contours with fewer than 5 points
+        if len(contour) >= 5:
+            # Fit an ellipse to the contour
+            ellipse = cv2.fitEllipse(contour)
+            
+            # Extract ellipse parameters
+            center, axes, angle = ellipse
+            major_axis, minor_axis = axes
 
-        for length, angle in segments:
-            x, y = current_point
-            x += length * np.cos(start_angle + angle)
-            y += length * np.sin(start_angle + angle)
-            current_point = (int(x), int(y))
-            points.append(current_point)
+            # Extend contours on both sides of the ellipse angle
+            extension_distance = 20  # Adjust the distance as needed
 
-        # Draw the curve in white color
-        color = (255, 255, 255)
-        is_closed = False  # Set to True if you want to close the 
-        thickness = np.random.randint(curve_thickness[0], curve_thickness[1])
-        cv2.polylines(img, [np.array(points)], isClosed=is_closed, color=color, thickness=thickness)
+            # Calculate points on the major and minor axes
+            major_axis_point = (
+                int(center[0] + extension_distance * np.cos(np.radians(angle))),
+                int(center[1] + extension_distance * np.sin(np.radians(angle)))
+            )
+            minor_axis_point = (
+                int(center[0] + extension_distance * np.cos(np.radians(angle + 90))),
+                int(center[1] + extension_distance * np.sin(np.radians(angle + 90)))
+            )
+
+            # Connect the points to create extended contours
+            extended_contour = np.array([major_axis_point, minor_axis_point], dtype=np.int32)
+            
+            # Draw the extended contour on the copy of the original image
+            cv2.polylines(img, [extended_contour], isClosed=False, color=(0, 255, 0), thickness=2)
 
     
-    black_spots_intensity = 0.02  # Adjust the intensity of the black spots
-    max_spot_radius = 20  # Adjust the maximum radius of the spots
-    num_clusters = 9  # Adjust the number of clusters
-    max_cluster_size =6000  # Adjust the maximum size of each cluster
-    cluster_intensity = 10  # Adjust the intensity of clustering
-
-    # Add black noise
-    cluster_positions = [(random.randint(0, img.shape[1]), random.randint(0, img.shape[0])) for _ in range(num_clusters)]
-    img_original = img.copy()
-    for cluster_center in cluster_positions:
-        # Generate points within each cluster
-        cluster_points = []
-        for _ in range(random.randint(max_cluster_size//3, max_cluster_size)):
-            x_offset = int(np.random.normal(0, cluster_intensity))
-            y_offset = int(np.random.normal(0, cluster_intensity))
-            x = min(max(0, cluster_center[0] + x_offset), img.shape[1] - 1)
-            y = min(max(0, cluster_center[1] + y_offset), img.shape[0] - 1)
-            cluster_points.append((x, y))
-
-        # Add points to the image
-        for point in cluster_points:
-            img[point[1], point[0]] = (0, 0, 0)
-
-    return img_original, img
-
-# Image size
-image_size = (214, 214)
-
-# Number of curves, maximum curve length, maximum curve angle, curve thickness, and black noise intensity
-num_curves = (2,6)
-max_curve_length = 210
-max_curve_angle = np.pi / 2  # 45 degrees
-curve_thickness = (3,9)
-black_noise_intensity = 0.02  # Adjust the intensity of the black noise
-
-# Create an image with random white curves and black noise
-original, random_curves_with_black_noise = draw_random_white_curves(image_size, num_curves, max_curve_length, max_curve_angle, curve_thickness, black_noise_intensity)
-
-# Display the result
-cv2.imshow('Random Curves with Black Noise', random_curves_with_black_noise)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    # side_by_side = np.hstack((img, gray))
+    cv2.imshow("Side by Side", img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    
+trace_walkable_sections('google_maps2.png')
