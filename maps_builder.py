f7bc13653242d2ee4099d4da87f53065cfde49b7
I549512 <amos.dinh@sap.com>
2023-12-20 18:13:56 +0100

    final

--- maps_builder.py
+++ maps_builder.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import sync_playwright
-
+import uuid
+import os
 def remove_all_background(page):
     remove = ["#omnibox-singlebox",".scene-footer-container", '.app-bottom-content-anchor','#play','.scene-footer-container', '#watermark','#titlecard', '#image-header']
     for selector in remove:
@@ -125,9 +126,16 @@ def click_on_interactive_street(page):
     # analyze the image, get all the walkable sections by color
     # read in the image as a numpy array
     blue_pixels = [0]
+    name = f"cache/google_maps_temp{str(uuid.uuid4())}.png"
+    while os.path.exists(name):
+        name = f"cache/google_maps_temp{str(uuid.uuid4())}.png"
+        
     for _ in range(5):
-        page.screenshot(path="google_maps_temp.png")
-        img = cv2.imread('google_maps_temp.png')
+        
+        page.screenshot(path=name)
+        img = cv2.imread(name)
+        # delete img
+        
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         blue_color = [3, 169, 244]
         blue_color2 = [18,158,175]
@@ -146,6 +154,8 @@ def click_on_interactive_street(page):
         
     if np.sum(blue_pixels) == 0:
         print('no blue pixels')
+        
+        
         return False
     else:
         # mask back to full image with 
@@ -176,22 +186,23 @@ def click_on_interactive_street(page):
         # while '[jsaction="compass.needle"]' not visible/ not on page
         while page.query_selector('[jsaction="compass.needle"]') == None:
             # if at least one in lower_center_img_mask
-            if len(upper_center_img_mask) > 0:
+            if len(right_img_mask[0]) > 0:
+                random_indices_with_1 = np.random.choice(np.arange(len(right_img_mask[0])))
+                random_indices_with_1 = right_img_mask[:, random_indices_with_1]
+            elif len(upper_center_img_mask[0]) > 0:
                 # click on random pixel in lower_center_img_mask which has a blue pixel
                 random_indices_with_1 = np.random.choice(np.arange(len(upper_center_img_mask[0])))
                 random_indices_with_1 = upper_center_img_mask[:, random_indices_with_1]
                 
-            elif len(lower_center_img_mask) > 0:
+            elif len(lower_center_img_mask[0]) > 0:
                 random_indices_with_1 = np.random.choice(np.arange(len(lower_center_img_mask[0])))
                 random_indices_with_1 = lower_center_img_mask[:, random_indices_with_1]
                 
-            elif len(left_img_mask) > 0:
+            elif len(left_img_mask[0]) > 0:
                 random_indices_with_1 = np.random.choice(np.arange(len(left_img_mask[0])))  
                 random_indices_with_1 = left_img_mask[:, random_indices_with_1]
                 
-            elif len(right_img_mask) > 0:
-                random_indices_with_1 = np.random.choice(np.arange(len(right_img_mask[0])))
-                random_indices_with_1 = right_img_mask[:, random_indices_with_1]
+           
             else:
                 random_indices_with_1 = np.random.choice(np.arange(len(all_img_mask[0])))
                 random_indices_with_1 = all_img_mask[:, random_indices_with_1]
@@ -201,71 +212,22 @@ def click_on_interactive_street(page):
             y = random_indices_with_1[0]
             page.mouse.click(int(x), int(y))
             page.wait_for_timeout(1000)
-    
-def launch_google_maps():
-    with sync_playwright() as p:
-        user_data_dir = '/home/amos/.config/google-chrome/Default/'
-        #user_data_dir = 'Profile 1'
-        # Launch the browser with the custom user data directory
-        # args = [
-            # '--disable-background-networking',
-            # '--no-sandbox',
-            # '--disable-setuid-sandbox',
-            # '--disable-infobars',#'--single-process',
             
-            # '--no-zygote',
-            # '--no-first-run',
-            # '--window-position=0,0',
-            # '--ignore-certificate-errors',
-            # '--ignore-certificate-errors-skip-list',
-            # '--disable-dev-shm-usage',
-            # '--disable-accelerated-2d-canvas',
-            # '--disable-gpu',
-            # '--hide-scrollbars',
-            # '--disable-notifications',
-            # '--disable-background-timer-throttling',
-            # '--disable-backgrounding-occluded-windows',
-            # '--disable-breakpad',
-            # '--disable-component-extensions-with-background-pages',
-            # '--disable-extensions',
-            # '--disable-features=TranslateUI,BlinkGenPropertyTrees',
-            # '--disable-ipc-flooding-protection',
-            # '--disable-renderer-backgrounding',
-            # '--enable-features=NetworkService,NetworkServiceInProcess',
-            # '--force-color-profile=srgb',
-            # '--metrics-recording-only',
-        #     # '--mute-audio']
-        # browser = p.chromium.launch_persistent_context(
-        #     '/usr/bin/google-chrome',
-        #     #user_data_dir=user_data_dir,
-        #     headless=False,
-        #     args=['--profile-directory=Default'],
-        #     # args=args,
-        #     #
-        #     # args=['---disable-blink-features=AutomationControlled',"--disable-dev-shm-usage",'--disable-component-extensions-with-background-pages'],
-        #     #args=browser_args,
-        #     #ignore_default_args=False,#['--enable-automation'],
-        #     #user_data_dir=user_data_dir
-        #     #executable_path='/usr/bin/google-chrome',
-        # )
+    
+def launch_google_maps(search_input):
+    # change playwright timeout
+    
+    
+    with sync_playwright() as p:
         browser = p.chromium.launch(headless=False)#args=['--profile-directory=Default'])
-
-        # Create a new page
         page = browser.new_page()
-
-        # Open a new page
-        #page = browser.new_page()
-        #page.set_extra_http_headers({"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"})
-
-        # Navigate to Google Maps
+        page.set_default_timeout(6000)
         page.goto("https://www.google.com/maps")
-        # alles akzeptieren
-        # timeout
         accept_button_selector = '[aria-label="Alle akzeptieren"]'
         page.click(accept_button_selector)
         
         search_input_name = '[name="q"]'
-        page.type(search_input_name, 'New York')
+        page.type(search_input_name, search_input)
         accept_button_selector = '[aria-label="Suche"]'
         page.click(accept_button_selector)
         page.wait_for_timeout(2000)
@@ -292,23 +254,44 @@ def launch_google_maps():
         # page.wait_for_timeout(2000)
         import uuid
         import datetime
-        def click_foto():
+        
+        def click_foto(is_first_click):
             def screenshot(name):
                 
                 page.screenshot(path=name)
                 
                 
-            name = 'images/'+datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")+str(uuid.uuid4())
+            name = 'images/'+datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")+str(uuid.uuid4())+'_'+'_'.join(search_input.split(' '))
             #remove_all_background(page)
             page.click('[jsaction="compass.needle"]')  # face north
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.scene-footer')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#watermark')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.app-vertical-widget-holder')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#omnibox-container')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[jsaction="focus:titlecard.main"]')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#minimap')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[jsaction="imageheader.close"]')
-            page.evaluate(f"""selector => document.querySelector(selector).parentElement.style.opacity = 0 """, '[jsaction="imageheader.close"]')
+            expressions = [
+                
+                '.scene-footer',
+                '#watermark',
+                '.app-vertical-widget-holder',
+                '#omnibox-container',
+                '[jsaction="focus:titlecard.main"]',
+                '#minimap',
+                '[jsaction="imageheader.close"]',
+                
+            ]
+            for expression in expressions:
+                try:
+                    page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, expression)
+                except:
+                    pass 
+            try:
+                page.evaluate(f"""selector => document.querySelector(selector).parentElement.style.opacity = 0 """, '[jsaction="imageheader.close"]')
+            except:
+                pass
+
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.scene-footer')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#watermark')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.app-vertical-widget-holder')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#omnibox-container')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[jsaction="focus:titlecard.main"]')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#minimap')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[jsaction="imageheader.close"]')
             
             
             page.wait_for_timeout(500)
@@ -332,23 +315,48 @@ def launch_google_maps():
                 
             page.wait_for_timeout(2000)
             #page.click('#widget-zoom-out')
-            steuerfeld = page.query_selector('[jsaction="drawer.close;mouseover:drawer.showToggleTooltip; mouseout:drawer.hideToggleTooltip;focus:drawer.showToggleTooltip;blur:drawer.hideToggleTooltip"]')
-            steuerfeld.click()
+            if is_first_click:
+                steuerfeld = page.query_selector('[jsaction="drawer.close;mouseover:drawer.showToggleTooltip; mouseout:drawer.hideToggleTooltip;focus:drawer.showToggleTooltip;blur:drawer.hideToggleTooltip"]')
+                steuerfeld.click()
+                is_first_click = False
             # opacity 0 using the steuerfeld handle
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[aria-label="Seitliches Steuerfeld einblenden"]')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[aria-label="Seitliches Steuerfeld einblenden"]')
             # opacity button 0
             #page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[jsaction="drawer.close;mouseover:drawer.showToggleTooltip; mouseout:drawer.hideToggleTooltip;focus:drawer.showToggleTooltip;blur:drawer.hideToggleTooltip"]')
             # #vasquette opacity 0
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#vasquette')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#vasquette')
             # aria-label="Interaktive Karte" opacity 0
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#layer')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#minimap')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.scene-footer')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.widget-layer-shown')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#watermark')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.app-vertical-widget-holder')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[aria-label="Street View-Abdeckung anzeigen"]')
-            page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#runway-expand-button')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#layer')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#minimap')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.scene-footer')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.widget-layer-shown')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#watermark')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '.app-vertical-widget-holder')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '[aria-label="Street View-Abdeckung anzeigen"]')
+            # page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, '#runway-expand-button')
+            
+            expressions = [
+                '[aria-label="Seitliches Steuerfeld einblenden"]',
+                '#vasquette',
+                '#layer',
+                '#minimap',
+                '.scene-footer',
+                '.widget-layer-shown',
+                '#watermark',
+                '.app-vertical-widget-holder',
+                '[aria-label="Street View-Abdeckung anzeigen"]',
+                '#runway-expand-button',
+                '[aria-roledescription="carousel"]'
+            ]
+            for expression in expressions:
+                try:
+                    page.evaluate(f"""selector => document.querySelector(selector).style.opacity = 0 """, expression)
+                except:
+                    pass
+            try:
+                page.evaluate(f"""selector => document.querySelector(selector).parentElement.parentElement.parentElement.parentElement.style.opacity = 0 """, '[jsaction="navigationrail.more"]')
+            except:
+                pass
             
            
             
@@ -373,13 +381,37 @@ def launch_google_maps():
             #page.click('#widget-zoom-out')
             page.wait_for_timeout(1000)
             click_on_interactive_street(page)
-        
+            
+        is_first_click = True
         while True:
-            click_foto()
+            click_foto(is_first_click)
+            is_first_click=False
         page.wait_for_timeout(50000000)
         
         
         browser.close()
 
 if __name__ == "__main__":
-    launch_google_maps()
+    import threading
+    # launch_google_maps(search_input='Palo Alto CA')
+    while True:
+        try: 
+            launch_google_maps(search_input='Palo Alto CA')
+        except:
+           pass
+        
+    # def worker(i):
+    #     print(f'Starting worker {i}')
+    #     launch_google_maps('San Francisco')
+    #     print(f'Finished worker {i}')
+
+    # num_workers = 2  # Change this to the number of workers you want
+
+    # threads = []
+    # for i in range(num_workers):
+    #     t = threading.Thread(target=worker, args=(i,))
+    #     threads.append(t)
+    #     t.start()
+
+    # for t in threads:
+    #     t.join()
